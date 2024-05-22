9758a9e88f4322acc11a6f0456dccfb9e6d3ba8e
MinHyeong <p610222@naver.com>
2024-05-22 19:22:18 +0900

    Update TileMap
    타일맵 생성

--- ProjectInGrapicClass/Assets/Scripts/Player.cs
+++ ProjectInGrapicClass/Assets/Scripts/Player.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [Header("플레이어 정보")]
     public float speed;
     public float jumpPower;
     Vector2 inputVec;
     Vector2 jumpVec;
 
+    [Header("충돌 처리")]
+    [SerializeField] LayerMask groundLayer;
+    RaycastHit2D hit;
+
+    [Header("플레이어 Component")]
     Rigidbody2D rigid;
     SpriteRenderer sprite;
     Animator anim;
@@ -22,6 +29,12 @@ public class Player : MonoBehaviour
     }
     private void FixedUpdate()
     {
+        Debug.DrawRay(rigid.position, Vector2.down,new Color(1, 0, 0), 0.5f);
+        if (Physics2D.Raycast(transform.position, Vector2.down, 0.5f, groundLayer))
+        {
+            Debug.Log("땅에 충돌");
+            anim.SetBool("isJump", false);
+        }
         // 이동 구현
         Move();
         // 점프 구현
@@ -49,7 +62,6 @@ public class Player : MonoBehaviour
     }
     void OnJump(InputValue value)
     {
-        //jumpVec = value.Get<Vector2>();
         jumpVec = Vector2.up;
     }
 }
